09b46cfad46662697b8698982c262474baccb943
mohamadburhan151 <mohamadburhan151@gmail.com>
2025-06-11 19:47:30 +0700

    add config vercel

--- backend/settings.py
+++ backend/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d*2jtccc5f71db(dav10!jyfnesdh89hb+elpra^@5f5ssepxj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
